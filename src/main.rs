47815f4a8c3550304cc122e16c0c882dd46a1dd5
Nik Graf <nik@nikgraf.com>
2018-08-09 14:55:47 +0200

    Switch to newest libs and get it to compile

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ extern crate rand;
 use ggez::{ContextBuilder, Context, GameResult};
 use ggez::conf::{WindowMode, WindowSetup};
 use ggez::event::{ self, EventHandler, Keycode, Mod} ;
-use ggez::graphics::{ Vector2 as gVector2, clear, circle, line, present, DrawMode, Point2 };
+use ggez::graphics::{ clear, circle, line, present, DrawMode, Point2 };
 use ggez::timer;
 
-use rand::Rng;
-
-use na::{Vector1, Vector2, Translation2, UnitComplex};
-use ncollide2d::shape::{Ball, Plane};
+use na::{Vector2, Isometry2};
+use ncollide2d::shape::{Ball, Cuboid, ShapeHandle};
+use ncollide2d::world::CollisionObjectHandle;
 use nphysics2d::world::World;
-use nphysics2d::math::Orientation;
-use nphysics2d::object::{RigidBody, BodyHandle};
+use nphysics2d::object::{RigidBody, Material, BodyHandle};
+use nphysics2d::volumetric::Volumetric;
 
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
@@ -37,6 +36,8 @@ const PLAYER_THRUST: f32 = 100.0;
 // Rotation in radians per second.
 const PLAYER_TURN_RATE: f32 = 3.0;
 
+const COLLIDER_MARGIN: f32 = 0.01;
+
 enum ActorType {
     Player,
     Wall,
@@ -44,10 +45,10 @@ enum ActorType {
 
 struct Actor {
     tag: ActorType,
-    body: RigidBody<f32>,
+    collider_handle: CollisionObjectHandle,
     pos: Point2,
     direction: f32,
-    velocity: gVector2,
+    velocity: Vector2<f32>,
     ang_vel: f32,
     mov_left: bool,
     mov_right: bool,
@@ -66,31 +67,49 @@ struct Assets {
 
 pub struct Game {
     world: World<f32>,
-    //TODO: turn player type into Actor instead of directly using BodyHandle<f32>
     player: Actor,
-    player_body: BodyHandle<f32>,
-    // walls: Vec<Actor>,
     is_walls_setup: bool,
     walls: Vec<Actor>,
-    wall_bodies: Vec<BodyHandle<f32>>,
     wall_collision: bool,
     wall_pos_col_x: f32,
     wall_pos_col_y: f32,
-    // level: i32,
-    // score: i32,
-    // assets: Assets,
-    // cubes: Vec<Cube>,
-    // timer: f64,
-    // temp: f64,
-    // ground_y: f64,
-    // mov_left: bool,
-    // mov_right: bool,
 }
 
-fn create_player() -> Actor {
+
+fn create_floor(world: &mut World<f32>) {
+    let ground_radx = 25.0;
+    let ground_rady = 1.0;
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector2::new(
+        ground_radx - COLLIDER_MARGIN,
+        ground_rady - COLLIDER_MARGIN,
+    )));
+
+    let ground_pos = Isometry2::new(-Vector2::y() * ground_rady, na::zero());
+    world.add_collider(
+        COLLIDER_MARGIN,
+        ground_shape,
+        BodyHandle::ground(),
+        ground_pos,
+        Material::default(),
+    );
+}
+
+fn create_player(world: &mut World<f32>) -> Actor {
+    let geom = ShapeHandle::new(Ball::new(PLAYER_R - COLLIDER_MARGIN));
+    let inertia = geom.inertia(1.0);
+    let center_of_mass = geom.center_of_mass();
+    let pos = Isometry2::new(Vector2::new(1.0, 0.0), 0.0);
+    let handle = world.add_rigid_body(pos, inertia, center_of_mass);
+    let collider_handle = world.add_collider(
+        COLLIDER_MARGIN,
+        geom.clone(),
+        handle,
+        Isometry2::identity(),
+        Material::default(),
+    );
     Actor {
         tag: ActorType::Player,
-        body: RigidBody::new_dynamic(Ball::new(PLAYER_R), 1.0, 0.0, 0.0),
+        collider_handle,
         pos: Point2::origin(),
         direction: 0.,
         velocity: na::zero(),
@@ -102,10 +121,25 @@ fn create_player() -> Actor {
     }
 }
 
-fn create_wall() -> Actor {
+fn create_wall(world: &mut World<f32>) -> Actor {
+    let geom = ShapeHandle::new(Cuboid::new(Vector2::new(
+        0.3 - COLLIDER_MARGIN,
+        0.6 - COLLIDER_MARGIN,
+    )));
+    let inertia = geom.inertia(1.0);
+    let center_of_mass = geom.center_of_mass();
+    let pos = Isometry2::new(Vector2::new(0.0, 1.0), 0.0);
+    let handle = world.add_rigid_body(pos, inertia, center_of_mass);
+    let collider_handle = world.add_collider(
+        COLLIDER_MARGIN,
+        geom.clone(),
+        handle,
+        Isometry2::identity(),
+        Material::default(),
+    );
     Actor {
         tag: ActorType::Wall,
-        body: RigidBody::new_static(Plane::new(Vector2::new(0.0, 1.0)), 0.3, 0.6),
+        collider_handle,
         pos: Point2::origin(),
         direction: 0.,
         velocity: na::zero(),
@@ -117,28 +151,33 @@ fn create_wall() -> Actor {
     }
 }
 
-fn create_walls(num: i32, blocked_pos: Point2) -> Vec<Actor> {
-    let new_wall = |_| {
-        let mut wall = create_wall();
-        let wall_angle = rand::random::<f32>() * 2.0 * std::f32::consts::PI;
-        // let wall_distance = rand:random::<f32>();
-        wall.pos = blocked_pos + vec_from_angle(wall_angle) * 2.0;
-        wall.velocity = random_vector(50.0);
-        wall
-    };
-    (0..num).map(new_wall).collect()
+fn create_walls(world: &mut World<f32>, num: i32, blocked_pos: Point2) -> Vec<Actor> {
+    // let new_wall = |_| {
+    //     let mut wall = create_wall(&mut world);
+    //     // let wall_angle = rand::random::<f32>() * 2.0 * std::f32::consts::PI;
+    //     // let wall_distance = rand:random::<f32>();
+    //     // wall.pos = blocked_pos + vec_from_angle(wall_angle) * 2.0;
+    //     // wall.velocity = random_vector(50.0);
+    //     wall
+    // };
+    let mut walls : Vec<Actor> = Vec::new();
+    for x in 0..num {
+        walls.push(create_wall(world));
+    }
+    walls
+    // (0..num).map(new_wall).collect()
 }
 
 /// Create a unit vector representing the
 /// given angle (in radians)
-fn vec_from_angle(angle: f32) -> ggez::graphics::Vector2 {
+fn vec_from_angle(angle: f32) -> Vector2<f32> {
     let vx = angle.sin();
     let vy = angle.cos();
-    ggez::graphics::Vector2::new(vx, vy)
+    Vector2::new(vx, vy)
 }
 
 /// Just makes a random `Vector2` with the given max magnitude.
-fn random_vector(max_magnitude: f32) -> ggez::graphics::Vector2 {
+fn random_vector(max_magnitude: f32) -> Vector2<f32> {
     let angle = rand::random::<f32>() * 2.0 * std::f32::consts::PI;
     let mag = rand::random::<f32>() * max_magnitude;
     vec_from_angle(angle) * (mag)
@@ -177,7 +216,8 @@ fn update_actor_position(actor: &mut Actor, dt: f32) {
     // let dv = ([[actor.velocity], [0, 1]].concat()) * (dt);
     let dv = actor.velocity * (dt);
     // println!("DIFF pos: {}", dv);
-    actor.pos += dv;
+    // TODO re-implement the pos change
+    // actor.pos += dv;
     // println!("CHANGE player pos: {}", actor.pos);
     actor.direction += actor.ang_vel;
 }
@@ -189,27 +229,16 @@ impl Game {
 
         let mut world = World::new();
         world.set_gravity(Vector2::new(0.0, 200.0));
-        let mut floor = RigidBody::new_static(Plane::new(Vector2::new(0.0, -1.0)), 0.0, 0.0);
-        floor.append_translation(&Translation2::new(0.0, FLOOR_Y));
-        world.add_rigid_body(floor);
 
-        let mut player = create_player();
-        let walls = create_walls(5, player.pos);
-        
-        let wall_bodies = Vec::new();
+        create_floor(&mut world);
+        let mut player = create_player(&mut world);
+        let walls = create_walls(&mut world, 5, player.pos);
 
-        // let mut player = RigidBody::new_dynamic(Ball::new(PLAYER_R), 1.0, 0.0, 0.0);
-        player.body.set_inv_mass(1.5);
-        player.body.append_translation(&Translation2::new(400.0, 300.0));
-        let player_body = world.add_rigid_body(player.body.clone());
-
-        let s = Game { 
+        let s = Game {
                 world, 
                 player, 
-                player_body, 
                 is_walls_setup: false,
                 walls, 
-                wall_bodies,
                 wall_collision: false,
                 wall_pos_col_x: 0.0,
                 wall_pos_col_y: 0.0,
@@ -217,34 +246,21 @@ impl Game {
         Ok(s)
     }
 
-    fn setup_walls(&mut self) {
-        if !self.is_walls_setup {
-            for wall in &mut self.walls {
-                wall.body.set_inv_mass(0.);
-                wall.body.append_rotation(&UnitComplex::new(100.0));
-                // wall.body.append_rotation(&UnitComplex::new(rand::thread_rng().gen_range(0.0, 100.0)));
-                wall.body.append_translation(&Translation2::new(0.0, rand::thread_rng().gen_range(200.0, 300.0))); //rand::thread_rng().gen_range(0., 400.0)));
-                self.wall_bodies.push(self.world.add_rigid_body(wall.body.clone()));
-            }
-            self.is_walls_setup = true;
-        }
-    }
-
-    fn refresh_horizontal_vel(&mut self) {
-        let mut player: std::cell::RefMut<RigidBody<f32>> = self.world.rigid_body(self.player_body);
-        let current = player.lin_vel();
-        let mut dir = 0.0;
-        let mut rot = 0.0;
-        if self.player.mov_left && !self.player.mov_right {
-            dir = -1.0;
-            rot = -800.0
-        } else if self.player.mov_right && !self.player.mov_left {
-            dir = 1.0;
-            rot = 800.0
-        }
-        player.set_lin_vel(Vector2::new(dir * 400.0, current.y));
-        player.set_rotation(UnitComplex::new(rot));
-    }
+    // fn refresh_horizontal_vel(&mut self) {
+    //     let mut player: std::cell::RefMut<RigidBody<f32>> = self.world.rigid_body(self.player_body);
+    //     let current = player.lin_vel();
+    //     let mut dir = 0.0;
+    //     let mut rot = 0.0;
+    //     if self.player.mov_left && !self.player.mov_right {
+    //         dir = -1.0;
+    //         rot = -800.0
+    //     } else if self.player.mov_right && !self.player.mov_left {
+    //         dir = 1.0;
+    //         rot = 800.0
+    //     }
+    //     player.set_lin_vel(Vector2::new(dir * 400.0, current.y));
+    //     player.set_rotation(UnitComplex::new(rot));
+    // }
 
     fn handle_collisions(&mut self) {
         // if player collides with wall, decide to stick to it with space/jump button
@@ -283,15 +299,13 @@ impl EventHandler for Game {
         while timer::check_update_time(ctx, DESIRED_FPS) {
             let seconds = 1.0 / (DESIRED_FPS as f32);
 
-            self.world.step(seconds);
+            self.world.step();
 
             player_handle_input(&mut self.player, seconds);
 
             // Update the pos of all actors to avoid e.g.: self.player.pos = [0, 0]
             update_actor_position(&mut self.player, seconds);
         }
-
-        self.setup_walls();
 
         self.handle_collisions();
 
@@ -300,26 +314,28 @@ impl EventHandler for Game {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         clear(ctx);
-        let pos = self.player_body.borrow().position_center();
-        circle(ctx, DrawMode::Fill, Point2::new(pos.x, pos.y), PLAYER_R, 0.1)?;
+        // TODO draw player based on physics engine
+        // let pos = self.player_body.borrow().position_center();
+        // circle(ctx, DrawMode::Fill, Point2::new(pos.x, pos.y), PLAYER_R, 0.1)?;
+        circle(ctx, DrawMode::Fill, Point2::new(0.5, 0.5), PLAYER_R, 0.1)?;
         line(ctx, &[Point2::new(0.0, FLOOR_Y), Point2::new(WINDOW_WIDTH as f32, FLOOR_Y)], 1.0)?;
 
-        // Loop over all objects drawing them...
-        {
-            // for wall in &self.walls {
-            //     let mut local = wall.borrow();
-            // }
+        // // Loop over all objects drawing them...
+        // {
+        //     // for wall in &self.walls {
+        //     //     let mut local = wall.borrow();
+        //     // }
 
-            for wall_body in &self.wall_bodies {
-                // let mut coords = wall_body.borrow_mut();
-                // let local = coords.local_to_world;
-                line(ctx, &[Point2::new(
-                    wall_body.borrow().position_center().x, 
-                    wall_body.borrow().position_center().y), 
-                    Point2::new(WINDOW_WIDTH as f32, 
-                    -100.0)], 1.0)?; //wall_body.borrow().position_center().coords.data
-            }
-        }
+        //     for wall_body in &self.wall_bodies {
+        //         // let mut coords = wall_body.borrow_mut();
+        //         // let local = coords.local_to_world;
+        //         line(ctx, &[Point2::new(
+        //             wall_body.borrow().position_center().x, 
+        //             wall_body.borrow().position_center().y), 
+        //             Point2::new(WINDOW_WIDTH as f32, 
+        //             -100.0)], 1.0)?; //wall_body.borrow().position_center().coords.data
+        //     }
+        // }
 
         present(ctx);
         Ok(())
@@ -341,14 +357,15 @@ impl EventHandler for Game {
                         self.world.set_gravity(Vector2::new(0.0, 200.0));
                     }
 
-                    let mut player: std::cell::RefMut<RigidBody<f32>> = self.player_body.borrow_mut();
-                    if player.position_center().y + PLAYER_R > FLOOR_Y - 0.1 {
-                        player.apply_central_impulse(Vector2::new(0.0, -150.0));
-                    }
+                    // TODO bring back jumping
+                    // let mut player: std::cell::RefMut<RigidBody<f32>> = self.player_body.borrow_mut();
+                    // if player.position_center().y + PLAYER_R > FLOOR_Y - 0.1 {
+                    //     player.apply_central_impulse(Vector2::new(0.0, -150.0));
+                    // }
                 }
                 _ => ()
             }
-            self.refresh_horizontal_vel();
+            // self.refresh_horizontal_vel();
         }
     }
     fn key_up_event(&mut self, _ctx: &mut Context, keycode: Keycode, _keymod: Mod, repeat: bool) {
@@ -373,7 +390,7 @@ impl EventHandler for Game {
                 }
                 _ => ()
             }
-            self.refresh_horizontal_vel();
+            // self.refresh_horizontal_vel();
         }
     }
 }
